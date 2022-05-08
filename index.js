3e0bc2b700d25f6170556ac46950c3f93e3c348a
hiromoo <hirokimt525@gmail.com>
2022-05-08 12:20:51 +0900

    Add a current SSID condition

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
+const wifi = require('node-wifi');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const user = process.env.ID;
-  const pass = process.env.PASS;
-  const url = process.env.URL;
-  await page.setExtraHTTPHeaders({Authorization: `Basic ${Buffer.from(`${user}:${pass}`).toString('base64')}`});
-  await page.goto(url);
+wifi.init({
+  iface: null
+});
 
-  await browser.close();
-})();
+wifi.getCurrentConnections((error, currentConnections) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const ssid = currentConnections[0].ssid;
+    if (ssid === process.env.SSID) {
+      (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const user = process.env.ID;
+        const pass = process.env.PASS;
+        const url = process.env.URL;
+        await page.setExtraHTTPHeaders({Authorization: `Basic ${Buffer.from(`${user}:${pass}`).toString('base64')}`});
+        await page.goto(url);
+
+        await browser.close();
+      })();
+    }
+  }
+});
